6e54eab7869cf1897797e16bf8a0e78c228ccb11
Ines CV <inescastelltort@gmail.com>
2019-04-27 11:29:19 +0200

    UPDATE get opinions from database

--- routes/opinions.js
+++ routes/opinions.js
@@ -1,51 +1,14 @@
 const express = require('express');
 const createError = require('http-errors');
-
 const router = express.Router();
 
 const Opinion = require('../models/opinion');
 
 // const { isLoggedIn } = require('../helpers/middlewares');
 
-router.get('/', (req, res, next) => {  
-  res.status(200).json([
-    { 
-      author: 'Paco is the author',
-      category: 'Nonsense',
-      question: 'Lo vamos a petar',
-      response: {
-        x: 'Claro que sí guapi',
-        y: 'Ni del Flais'
-      }
-    },
-    { 
-      author: 'Made by UOP',
-      category: 'Nonsense',
-      question: 'su PUTA MADRE',
-      response: {
-        x: 'Claro que sí guapi',
-        y: 'Ni del Flais'
-      }
-    },
-    { 
-      author: 'Made by UOP',
-      category: 'Nonsense',
-      question: 'Has matado a Jorge, tia controlate, lo necesitas para el proyecto',
-      response: {
-        x: 'Nah, a la mierda todo',
-        y: 'Ostias, verdad'
-      }
-    },
-    { 
-      author: 'Made by UOP',
-      category: 'Nonsense',
-      question: 'Si tienes que matar a una persona, ¿a quién matarias?',
-      response: {
-        x: 'Ines, está claro',
-        y: 'Jorge, no doubt'
-      }
-    },
-  ]);
+router.get('/', async (req, res, next) => {  
+  const opinions = await Opinion.find();
+  res.status(200).json(opinions);
 });
 
 router.get('/categories', (req, res, next) => {
