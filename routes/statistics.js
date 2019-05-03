32cd57938e3e55de5f495081b8d49b10953560cd
JdeJ <jorgedjuana@gmail.com>
2019-05-03 16:00:43 +0200

    UPDATE remove onipinion when responded

--- routes/statistics.js
+++ routes/statistics.js
@@ -8,51 +8,27 @@ const Response = require('../models/response');
 
 const { isLoggedIn } = require('../helpers/middlewares');
 
-router.post('/', (req, res, next) => {
+router.use(isLoggedIn('user'));
+
+router.post('/', async (req, res, next) => {
   const query = req.body;
-  console.log(query);
+
+  switch (Object.keys(query)[0]) {
+    case 'category':
+      console.log('category');
+      break;
+    case 'user':
+      console.log('user');
+      break;
+    case 'opinion':
+      const responses = await Response.find();
+      console.log(responses);
+      break;
+    default:
+  }
+
   const result = { estado: 'todo chachi' };
   res.status(200).json(result);
 });
-
-// router.get('/opinions', async (req, res, next)=> {
-
-// });
-
-// router.get('/opinions/opinion/:id', async (req, res, next) => {
-//   const { id: user } = req.params;
-
-//   try {
-//     const { username, description, avatar, role } = await User.findById(user);
-//     const opinions = await Opinion.find({ author: { $in: [user] } }).select('category question response -_id');
-//     // const responses = await Response.find({ author: { $in: [user] } }).select('category question response -_id');
-
-
-//     res.status(200).json({
-//       message: 'User data returned succesfully',
-//       user: {
-//         username,
-//         description,
-//         avatar,
-//         role,
-//         opinions,
-//         request: {
-//           type: 'GET',
-//           url: `${process.env.HEROKU}/users/${user}`,
-//         },
-//       },
-//     });
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-
-// router.get('/opinions/categories', async (req, res, next)=> {
-
-// });
-
-// router.get('/opinions/categories/:id', async (req, res, next)=> {
-
-// });
 
 module.exports = router;
