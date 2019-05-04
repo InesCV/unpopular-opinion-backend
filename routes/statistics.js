f27c25ccb7966cfa87697d6bdf327561c69ed95c
Ines CV <inescastelltort@gmail.com>
2019-05-04 17:53:09 +0200

    UPDATE statistics

--- routes/statistics.js
+++ routes/statistics.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-const User = require('../models/user');
-const Opinion = require('../models/opinion');
+// const User = require('../models/user');
+// const Opinion = require('../models/opinion');
 const Response = require('../models/response');
 
 const { isLoggedIn } = require('../helpers/middlewares');
@@ -13,7 +13,9 @@ router.use(isLoggedIn('user'));
 router.post('/', async (req, res, next) => {
   const query = req.body;
   let data = null;
-  let stat = 1;
+  let stat = 0;
+  let responses = null;
+  let responseIndex = null;
 
   if (!Object.prototype.hasOwnProperty.call(query, 'user')) {
     query.user = req.session.currentUser._id;
@@ -29,11 +31,12 @@ router.post('/', async (req, res, next) => {
     case 'opinion':
       break;
     case 'opinionRate':
-      const responses = await Response.find({ 'opinion': query.opinion });
-      const responseIndex = responses.findIndex((resp)=>{
-        return resp.user.equals(query.user);
-      });
-      if (responseIndex > 0) {
+      // Find all responses to an specific opinion
+      responses = await Response.find({ opinion: query.opinion });
+      // Find what the user has responded to that specific opinion
+      responseIndex = responses.findIndex(resp => resp.user.equals(query.user));
+      // Count how many people have responded the same as the user
+      if (responseIndex >= 0) {
         stat = responses.reduce((cont, resp) => {
           if (resp.response == responses[responseIndex].response) {
             return cont + 1;
@@ -41,6 +44,7 @@ router.post('/', async (req, res, next) => {
           return cont;
         }, 0);
       }
+      // Calculate the % of the people that has responded the same as the user
       stat = (stat / responses.length) * 100;
       data = {
         message: 'Opinion statistics',
