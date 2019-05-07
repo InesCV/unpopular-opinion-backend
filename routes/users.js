5e50782bb2cd62d759b16593e8967ba447e2cb10
Ines CV <inescastelltort@gmail.com>
2019-05-07 19:00:21 +0200

    ADD user id query

--- routes/users.js
+++ routes/users.js
@@ -12,22 +12,15 @@ router.get('/:id', async (req, res, next) => {
   const { id: user } = req.params;
 
   try {
-    const { username, description, avatar, role } = await User.findById(user);
-    const opinions = await Opinion.find({ author: { $in: [user] } }).select('category question response -_id');
-    const responses = await Response.find({ author: { $in: [user] } }).select('category question response -_id');
-
+    const { username, description, avatar } = await User.findById(user);
+    const opinions = await Opinion.find({ author: { $in: [user] } }).select('question response -_id');
     res.status(200).json({
       message: 'User data returned succesfully',
       user: {
         username,
         description,
         avatar,
-        role,
         opinions,
-        // request: {
-        //   type: 'GET',
-        //   url: `${process.env.HEROKU}/users/${user}`,
-        // },
       },
     });
   } catch (error) {
