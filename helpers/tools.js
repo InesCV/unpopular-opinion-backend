8921c7aa9b1f0eb9702b4ca546aeb5a5a64f2f24
JdeJ <jorgedjuana@gmail.com>
2019-05-17 17:34:34 +0200

    ADD user location

---
+++ helpers/tools.js
@@ -0,0 +1,47 @@
+const User = require('../models/user');
+
+module.exports = {
+  // findUserSocket: async (id, sockets) => { sockets.find(u => u.userId === id); },
+  findUserSocket: async (id, sockets) => { sockets.findIndex(x => x.id === id); },
+
+  updateUserPosition: async (userId, position) => {
+    const user = await User.findOne({ _id: userId });
+    if (!(user.position.coordinates[0] === position[0] && user.position.coordinates[1] === position[1])) {
+      user.position = {
+        type: 'Point',
+        coordinates: position,
+      };
+      await user.save();
+    }
+  },
+
+  updateDbPosition: async (userId, position) => {
+    const user = await User.findOne({ _id: userId });
+
+    user.position = {
+      type: 'Point',
+      coordinates: position,
+    };
+    await user.save();
+
+    return user;
+  },
+
+  findNearOpiners: async (userId) => {
+    const user = await User.findOne({ _id: userId });
+
+    const opiners = await User.find({
+      position: {
+        $near: {
+          $geometry: {
+            type: 'Point',
+            coordinates: [user.position.coordinates[0], user.position.coordinates[1]],
+          },
+          $maxDistance: 400,
+          $minDistance: 0,
+        },
+      },
+    });
+    return opiners;
+  },
+};
